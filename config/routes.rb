bd3964ce0c9704929fec5e33bc261108cbd5531e
Linda Lai <lindalai.615@gmail.com>
2018-11-10 23:45:14 +1100

    Created puppergram info page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/info'
   resources :puppers
   root to: redirect('/ideas')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
